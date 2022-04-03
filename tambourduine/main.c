f6bf474a80bd2c4d4170319905284511784c7661
Nasser <nasserbrown@estudiantec.cr>
2022-04-03 17:43:46 -0600

    Finished rutines

--- tambourduine/main.c
+++ tambourduine/main.c
@@ -2,8 +2,21 @@
 #include "tambourduine.h"
 int main()
 {
-    const float tempo = 2000;
-    start();
-    play(tempo);
+    const float tempo = 500;
+    start(tempo,5);
+    fan("A");
+    fan("B");
+    vertical("D");
+    vertical("I");
+    percussion("D");
+    percussion("I");
+    percussion("DI");
+    percussion("A");
+    percussion("B");
+    percussion("AB");
+    hit();
+    vibrato();
+    routine(1788,1400,1000);
+    
     return 0;
 }
